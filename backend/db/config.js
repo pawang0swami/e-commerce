ba2564d5de59256cece8d9c03abf1bdd2ed63353
PAWAN GOSWAMI <pawangoswami6823@gmail.com>
2025-04-08 11:36:42 +0530

    mongoodb

--- backend/db/config.js
+++ backend/db/config.js
@@ -1,3 +1,6 @@
 const mongoose = require("mongoose");
 
 mongoose.connect("mongodb+srv://pawangoswami6823:goswami%406823@ecom.drtu1.mongodb.net/e-comm");
+// mongoose.connect("mongodb://localhost:27017/e-comm");
+
+
