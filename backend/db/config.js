aae971ecc3c2352faf8c9ae1f705b102aa93c8b0
PAWAN GOSWAMI <pawangoswami6823@gmail.com>
2025-05-23 12:07:59 +0530

    hi

--- backend/db/config.js
+++ backend/db/config.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-
-mongoose.connect("mongodb+srv://pawangoswami6823:goswami%406823@ecom.drtu1.mongodb.net/e-comm");
+mongoose.connect("mongodb+srv://mohitgoswami11115:AIiMkLv7qBY4uKV0@cluster1.vf9wzpk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1")
+// mongoose.connect("mongodb+srv://pawangoswami6823:goswami%406823@ecom.drtu1.mongodb.net/e-comm");
 // mongoose.connect("mongodb://localhost:27017/e-comm");
 
 
